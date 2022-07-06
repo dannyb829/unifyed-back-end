7094a788f00bbaa20a8a4154eb5968f8c04ccb80
Daniel <dannyb829@gmail.com>
2022-07-06 15:20:54 -0400

    mailer url adjustment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,13 +59,12 @@ Rails.application.configure do
   
   # Uncomment if you wish to allow Action Cable access from any origin.
   config.action_cable.disable_request_forgery_protection = true
-  config.action_cable.url = 'ws://localhost:3000/cable'
 
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => 'uni-fyed.herokuapp.com', protocol: 'http' }
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
